a3eddd9f000b47ac2d499340d21ff5c41ef7f5ab
danielmicek <micekdaniel123@gmail.com>
2026-03-25 13:43:56 +0100

    commit no.46: docker and containerization changes

--- E-Learning-App/backend/database.js
+++ E-Learning-App/backend/database.js
@@ -1,13 +1,16 @@
 // make connection to the dbs
-import { Pool } from "pg";
+import {Pool} from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const pool = new Pool({
-    user: "postgres",
-    password: "postgrePWD3633",
-    host: "localhost",
-    port: 5432,
-    database: "dbs"
-})
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    database: process.env.DB_NAME,
+});
 
 
 export default pool;
